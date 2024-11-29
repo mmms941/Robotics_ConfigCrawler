5fb0cc21f30aeee05410614bc5fbace99b7e2573
MM <mh.mot1380@gmail.com>
2024-11-29 16:48:01 +0330

    Time Change

--- run_script.sh
+++ run_script.sh
@@ -8,6 +8,6 @@ python3 /root/Robotics_ConfigCrawler/main.py
 
 # اضافه کردن و پوش کردن فایل‌ها به گیت هاب
 cd /root/Robotics_ConfigCrawler
-git add configs.txt "tg channels.json" "blacklist channels.json"
+git add index.html configs.txt "tg channels.json" "blacklist channels.json"
 git commit -m "Auto Hourly Update: $(date)"
 git push origin main
